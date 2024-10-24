4bbefd81431598a0ef7aa67f297327eb33838016
kodawantsasoda <dkotarvera@gmail.com>
2024-10-24 08:02:43 -0700

    RTC Implemetation
    RTC has been implemented along with PETHUD changes via the RTC reacting with PetVars.

--- Assets/scripts/RTC/RTCScript.cs
+++ Assets/scripts/RTC/RTCScript.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RTCScript : MonoBehaviour
 {
@@ -14,6 +16,14 @@ public class RTCScript : MonoBehaviour
     public string currentTime;
     public string pastTime;
 
+    bool newDay;
+
+    public petVars pet;
+    public missionController missions;
+
+    public TextMeshProUGUI affectionText;
+    public TextMeshProUGUI critterCoinText;
+    public TextMeshProUGUI hungerText;
 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +31,18 @@ public class RTCScript : MonoBehaviour
         current = System.DateTime.Now;
         hourTracker = current;
         minuteTracker = current;
+        newDay = false;
         loadOldTime();
         calculateTimeAway();
     }
 
     private void OnDestroy()
     {
+        //saving new old time
         old = current;
         PlayerPrefs.SetString("SavedDateTime", old.ToString("o"));
         PlayerPrefs.Save();
+        Debug.Log(PlayerPrefs.GetString("SavedDateTime").ToString());
     }
 
     // Update is called once per frame
@@ -37,24 +50,44 @@ public class RTCScript : MonoBehaviour
     {
         current = System.DateTime.Now;
 
-        //midnight reset
-        if(current.Hour == 24)
+        //resetting @ midnight
+        if(newDay == false)
         {
-            //reset daily missions
-            //affection -100 every 24 hours
+            if(current.Hour == 7)
+            {
+                Debug.Log("WAT");
+                //reset daily missions
+                missions.resetProgress();
+                missions.assignDailyMissions();
+
+                //affection -100 @ midnight
+                pet.affection = 0;
+                newDay = true;
+            }
         }
 
-        if(current.Hour - hourTracker.Hour == 1)
+        if(newDay == true)
+        {
+            if(current.Hour != 7)
+            {
+                newDay = false;
+            }
+        }
+
+        if (current.Hour - hourTracker.Hour != 0)
         {
             //feed -10
             hourTracker = current;
+            decreaseHungerbyTime(1);
             Debug.Log("1 hour has passed! +10 hunger!");
         }
 
-        if(current.Minute - minuteTracker.Minute == 1)
+        if(current.Minute - minuteTracker.Minute != 0)
         {
             //critter coin increase
             minuteTracker = current;
+            pet.critterCoin++;
+            updatePetHUD(critterCoinText, pet.critterCoin.ToString());
             Debug.Log("1 minute has passed! +1 Critter Coin!");
         }
     }
@@ -68,6 +101,8 @@ public class RTCScript : MonoBehaviour
             {
                 calculatedHour = current.Hour - old.Hour;
                 //hunger = hunger + calculated hour * 10
+                decreaseHungerbyTime(calculatedHour);
+                
             }
         }
         else
@@ -85,6 +120,7 @@ public class RTCScript : MonoBehaviour
         if (!string.IsNullOrEmpty(savedDateTime))
         {
             old = DateTime.Parse(savedDateTime, null, System.Globalization.DateTimeStyles.RoundtripKind);
+            Debug.Log("Loaded DateTime: " + old);
         }
         else
         {
@@ -92,5 +128,17 @@ public class RTCScript : MonoBehaviour
         }
     }
 
-    
+    public void decreaseHungerbyTime(int numHours)
+    {
+        pet.hunger = pet.hunger - numHours * 10;
+        if(pet.hunger < 0)
+        {
+            pet.hunger = 0;
+        }
+    }
+
+    public void updatePetHUD(TextMeshProUGUI text, string newText)
+    {
+        text.text = newText;
+    }
 }
